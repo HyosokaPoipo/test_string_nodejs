2ab632c8c83653b81b69171727eb07c0852adc2c
Maryadi <maryadi@astrnt.co>
2017-12-10 11:16:23 +0700

    add space slitter

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 /*** Chek String Occurence withing string ***/
 var test1 = "thanks @syifa_ullinnas",
 test2 = "thanks @heytacho, @syifa_ullinnas",
-test3 = "thanks @syifa_ullinnas thanks @heytacho";
+test3 = "thanks @syifa_ullinnas dan sdfasdfsadf thanks @heytacho asdfsdf";
 
-var thanks_index = test3.indexOf("thanks @");
+var thanks_index = test2.indexOf("thanks @");
 console.log(thanks_index);
 
 
-var thanks_index2 = test1.split('thanks @');
+var thanks_index2 = test3.split('thanks @');
 
 thanks_index2.forEach(function (item) {
-    console.log(item);
+    var temp = item.split(" ");
+    console.log(temp[0]);
 });
 
 /*********************************************/
